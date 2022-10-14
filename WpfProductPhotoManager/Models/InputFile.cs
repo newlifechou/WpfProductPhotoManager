b311e3aeba7cc68eee74644715b0a63cfb467fba
xszhou <newlifechou@outlook.com>
2022-10-15 05:00:37 +0800

    create git control

---
+++ WpfProductPhotoManager/Models/InputFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfProductPhotoManager.Models
+{
+    public class InputFile
+    {
+        public InputFile()
+        {
+            IsCopied = false;
+            IsUploaded = false;
+        }
+        public string OrignalFileName { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public bool IsCopied { get; set; }
+        public string CopyError { get; set; }
+        public string NewFileName { get; set; }
+
+        public bool IsUploaded { get; set; }
+        public string UploadError { get; set; }
+    }
+}
